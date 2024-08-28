b1d9f9e0bc73a828cc8d638776b754a42518b44e
toji <toji@192.168.1.4>
2024-08-28 12:23:45 +0300

    switch from moving to creating symlinks in install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/sh
-cp -r hypr nvim waybar ~/.config/
-cp zshrc ~/.zshrc
+HERE="/home/toji/dotfiles"
+
+ln -s "$HERE/hypr" /home/toji/.config/hypr
+ln -s "$HERE/nvim" /home/toji/.config/nvim
+ln -s "$HERE/waybar" /home/toji/.config/waybar
+
+ln -s "$HERE/zshrc" /home/toji/.zshrc
 
 mkdir -p ~/.config/kitty
-cp kitty.conf ~/.config/kitty
+ln -s "$HERE/kitty.conf" /home/toji/.config/kitty/kitty.conf
 
 mkdir -p ~/.config/alacritty
-cp alacritty.toml ~/.config/alacritty/
+ln -s "$HERE/alacritty.toml" /home/toji/.config/alacritty/alacritty.toml
+
 
 mkdir -p ~/.config/rofi
-cp -r rofi ~/.config/
+ln -s "$HERE/rofi" /home/toji/.config/rofi
 
 mkdir -p ~/.config/Vencord
 mkdir -p ~/.config/Vencord/themes
-cp ./Vencord/themes/midnight.theme.css ~/.config/Vencord/themes
+ln -s "$HERE/Vencord/themes/midnight.theme.css" /home/toji/.config/Vencord/themes/midnight.theme.css
