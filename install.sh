e07ad4c5989116dc296679fba1214ab4eb0b78c2
xfcasio <xfc1@pm.me>
2024-09-06 01:56:49 +0300

    add username flexibility to install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/sh
-HERE="/home/toji/dotfiles"
+HERE="/home/$USER/dotfiles"
 
-ln -s "$HERE/hypr" /home/toji/.config/hypr
-ln -s "$HERE/nvim" /home/toji/.config/nvim
-ln -s "$HERE/waybar" /home/toji/.config/waybar
+ln -s "$HERE/hypr" "/home/$USER/.config/hypr"
+ln -s "$HERE/nvim" "/home/$USER/.config/nvim"
+ln -s "$HERE/waybar" "/home/$USER/.config/waybar"
 
-ln -s "$HERE/zshrc" /home/toji/.zshrc
+ln -s "$HERE/zshrc" "/home/$USER/.zshrc"
 
 mkdir -p ~/.config/kitty
-ln -s "$HERE/kitty.conf" /home/toji/.config/kitty/kitty.conf
+ln -s "$HERE/kitty.conf" "/home/$USER/.config/kitty/kitty.conf"
 
 mkdir -p ~/.config/alacritty
-ln -s "$HERE/alacritty.toml" /home/toji/.config/alacritty/alacritty.toml
+ln -s "$HERE/alacritty.toml" "/home/$USER/.config/alacritty/alacritty.toml"
 
 
 mkdir -p ~/.config/rofi
-ln -s "$HERE/rofi" /home/toji/.config/rofi
+ln -s "$HERE/rofi" "/home/$USER/.config/rofi"
 
 mkdir -p ~/.config/Vencord
 mkdir -p ~/.config/Vencord/themes
-ln -s "$HERE/Vencord/themes/midnight.theme.css" /home/toji/.config/Vencord/themes/midnight.theme.css
+ln -s "$HERE/Vencord/themes/midnight.theme.css" "/home/$USER/.config/Vencord/themes/midnight.theme.css"
