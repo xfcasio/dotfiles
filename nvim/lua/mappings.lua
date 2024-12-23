0ed5a5f25337737dfc9becb85f12d0129be923b0
xfcisco <xfc1@pm.me>
2024-12-23 22:26:40 +0200

    [nord theme] add verticle <A-l> terminal to nvim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,8 +9,14 @@ local terminal = require("nvterm.terminal")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
 map({'n', 't'}, '<A-k>', function ()
   require("nvchad.term").toggle { pos = "sp", size = 0.7 }
 end)
+
+map({'n', 't'}, '<A-l>', function ()
+  require("nvchad.term").toggle { pos = "vsp", size = 0.5 }
+end)
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
