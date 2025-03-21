e7106006431048074bd972a8c5647b6f227bd695
xfcisco <xfc1@pm.me>
2025-03-21 04:40:25 +0200

    [blue clouds] some cleanup

--- modify-rxyhn.sh
+++ modify-rxyhn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find '/home/toji/.local/share/nvim/' -type f -exec sed -i 's/131e22/020d11/g' '{}' \;
-find '/home/toji/.local/share/nvim/' -type f -exec sed -i 's/D9D7D6/A9A9A9/g' '{}' \;
+find "/home/$USER/.local/share/nvim/" -type f -exec sed -i 's/131e22/020d11/g' '{}' \;
+find "/home/$USER/.local/share/nvim/" -type f -exec sed -i 's/D9D7D6/A9A9A9/g' '{}' \;
